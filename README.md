9598f6eefc8c51422f2b00824e8280607b384954
Lukeeeeee <lukedong123@gmail.com>
2017-03-22 16:22:29 +0800

    local change

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ### GARIC 
-### reference http://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=159061
+### Reference http://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=159061
--- src/fuzzyLogicRule/fuzzyRule.py
+++ src/fuzzyLogicRule/fuzzyRule.py
@@ -6,10 +6,14 @@ class FuzzyRule(object):
 	def __init__(self,
 				input_val_list,
 				output_val,
-				rule = None
+				rule,
+				section
 				):
 		self.input_val_list = input_val_list
 		self.output_val = output_val
 		self.rule_string = rule
+		self.section = section
+
 	def reason(self, input_val_list):
+
 		pass
