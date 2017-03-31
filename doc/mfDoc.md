88ca14565f3c0ec938b06ccfd0c7e0b5aeaf0c2d
Lukeeeeee <lukedong123@gmail.com>
2017-03-31 11:46:13 +0800

    inital controller class

---
+++ doc/mfDoc.md
@@ -0,0 +1,11 @@
+### Memebership Function
+#### input: a value or a degree
+#### output: a degree or value
+
+#### attribute
+1. name
+2. parameter: eg. for triangle c, sl, sr
+
+#### method
+1. calc() //calc a degree of a real value
+2. inverse() //calc a value with a degree
--- src/controller/controller.py
+++ src/controller/controller.py
@@ -3,4 +3,16 @@ from  src.fuzzyLogicRule import *
 
 class Controller(object):
     def __init__(self, name):
+        self.input = {}
+        self.output = {}
+        self.rule_section_set = {}
+        pass
+
+    def add_rule_section(self, rule_set):
+        pass
+
+    def assign_input_to_secion(self):
+        pass
+
+    def calc_output(self):
         pass
