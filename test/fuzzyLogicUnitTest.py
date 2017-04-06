efee1e1d6d57d6e53d525e7533b908e053d4466b
Lukeeeeee <lukedong123@gmail.com>
2017-04-06 17:00:35 +0800

    Try to design data importer.

--- test/fuzzyLogicUnitTest.py
+++ test/fuzzyLogicUnitTest.py
@@ -55,7 +55,6 @@ def construct_controller(rule_set = None):
     if rule_set is not None:
         controller.add_rule_section(rule_set)
     return controller
-    pass
 
 
 class fuzzyLogicRuleTest(unittest.TestCase):
