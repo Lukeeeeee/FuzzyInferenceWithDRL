3f98e84b9424c0ba4d6dffb011edf5051ce6dade
zhongwang <18279946194@163.com>
2017-04-18 16:53:01 +0800

    test

---
+++ test/initializationTest/generate_dataset.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+from ddpgInitializationTest import *
+import json
+
+if __name__ == '__main__':
+    env = Environment(name="InitialTest", state_dim=100, action_dim=10)
+
+    testInitializer = ddpgInitializaionTest(state_dim=env.state_dim,
+                                            action_dim=env.action_dim)
+    fuzzy_controller = testInitializer.generate_fuzzy_logic_controller()
+    fuzzy_valuer = testInitializer.generate_fuzzy_logic_valuer()
+
+    ddpgController = DDPGController(env)
+
+    ddpgInitializer = DDPGInitializer(ddpg_controller=ddpgController,
+                                      fuzzy_logic_controller=fuzzy_controller,
+                                      fuzzy_logic_valuer=fuzzy_valuer,
+                                      mini_batch_size=100)
+
+    dataset_log = open("../../log/dataset.json", "w")
+    json.dump(ddpgInitializer.generate_training_sample_mini_batch(10), dataset_log)
